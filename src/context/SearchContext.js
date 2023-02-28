f42845806f1e52c48248e26479dfb2ff6757546c
draktate <draktate@gmail.com>
2023-02-28 20:24:11 +0530

    first commit

---
+++ src/context/SearchContext.js
@@ -0,0 +1,62 @@
+import { createContext, useReducer } from "react";
+
+const INITIAL_STATE = {
+  city: undefined,
+  dates: [],
+  options: {
+    adult: undefined,
+    children: undefined,
+    room: undefined,
+  },
+  api_redirect:"http://dnyweb.ap-south-1.elasticbeanstalk.com/api/"
+};
+
+export const SearchContext = createContext(INITIAL_STATE);
+
+const SearchReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_SEARCH":
+      return action.payload;
+    case "RESET_SEARCH":
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
+
+export const SearchContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(SearchReducer, INITIAL_STATE);
+
+  return (
+    <SearchContext.Provider
+      value={{
+        city: state.city,
+        dates: state.dates,
+        options: state.options,
+        api_redirect:"http://dnyweb.ap-south-1.elasticbeanstalk.com/api",
+        dispatch,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
